96235712077bbd696f2046afa6121f7851e2a11b
Derek <14247289+derek022@users.noreply.github.com>
2021-12-19 23:11:48 +0800

    底层通信框架

---
+++ LOLServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LOLServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
